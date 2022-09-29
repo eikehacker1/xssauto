c78716235f05c1e020f7a77655bbd20e254873d1
EIKE Oliveira <101583887+eikehacker1@users.noreply.github.com>
2022-09-29 09:50:35 -0300

    Add files via upload
    xssauto htts://psexemplo.com/?s=<script>alert(1)</script>
    
    ou
    
     cat exemplo.txt xssouto

---
+++ xssaut.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"sync"
+	"bufio"
+	"net/http"
+	"fmt"
+	"os"
+	"strings"
+	"io/ioutil"
+)
+
+func main(){
+	// fmt.Println("\n")
+	fmt.Println("bem vindo ao colador")
+	// fmt.Println("\n")
+
+	colorReset := "\033[0m"
+	colorRed := "\033[31m"
+    colorGreen := "\033[32m"
+
+
+	sc := bufio.NewScanner(os.Stdin)
+
+	jobs := make(chan string)
+	var wg sync.WaitGroup
+
+	for i:= 0; i < 20; i++{
+
+		wg.Add(1)
+		go func(){
+			defer wg.Done()
+			for domain := range jobs {
+
+				resp, err := http.Get(domain)
+				if err != nil{
+					continue
+				}
+				body, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+	      			fmt.Println(err)
+	   			}
+	   			sb := string(body)
+	   			check_result := strings.Contains(sb , "alert(1)")
+	   			// fmt.Println(check_result)
+	   			if check_result != false {
+	   				fmt.Println(string(colorRed),"Vuneravel a xss:", domain,string(colorReset))
+	   			}else{
+	   				fmt.Println(string(colorGreen),"Nao vulneravel a xss:", domain, string(colorReset))
+	   			}
+
+			}
+
+   		}()
+
+	}
+
+
+
+	for sc.Scan(){
+		domain := sc.Text()
+		jobs <- domain
+
+
+	}
+	close(jobs)
+	wg.Wait()
+}
